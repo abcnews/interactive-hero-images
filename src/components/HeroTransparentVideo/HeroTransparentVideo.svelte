db229779b200a1a822dc3085590f6829257ac9e5
Ash Kyd <kyd.ashley@abc.net.au>
2026-06-01 15:57:16 +1000

    bug: some computers show black borders around vids

--- src/components/HeroTransparentVideo/HeroTransparentVideo.svelte
+++ src/components/HeroTransparentVideo/HeroTransparentVideo.svelte
@@ -6,16 +6,33 @@
 
 <SharedStyles />
 
-<video class="vid" muted autoplay loop playsinline style:max-width={width} {width} {height}>
-  <source src={vidSafari} type="video/mp4;codecs=hvc1" />
-  <source src={vid} type="video/webm" />
-</video>
+<div class="video-wrapper" style:max-width={width}>
+  <video muted autoplay loop playsinline {width} {height}>
+    <source src={vidSafari} type="video/mp4;codecs=hvc1" />
+    <source src={vid} type="video/webm" />
+  </video>
+</div>
 
 <style lang="scss">
-  .vid {
+  .video-wrapper {
+    overflow: hidden;
+    transform: translateZ(0);
+    will-change: transform;
+
+    /* Custom sizing code */
     width: 75vw;
     height: auto;
     margin: 0 auto;
     max-width: 400px;
+
+    video {
+      object-fit: cover;
+      transform: translateZ(0);
+      display: block;
+      width: calc(100% + 2px);
+      height: calc(100% + 2px);
+      margin: -1px;
+      object-fit: cover;
+    }
   }
 </style>
